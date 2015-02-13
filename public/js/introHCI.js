5a071b7764bb033e2573e0566fbb91db2fd6a816
Alexes Macedo <a1macedo@ucsd.edu>
2015-02-13 13:38:10 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,20 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, function(result) {
+		console.log(result);
+		var htmlDetails = '<a href="#" class="detailsImage">' +
+		'<img src="' + result['image'] + '" class="img">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>';
+
+		$("#" + projectID + " div.details").html(htmlDetails);
+		$("#" + projectID + " div.details").html(result['summary']); 
+	});
+	console.log("User clicked on /project/" + idNumber);
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +48,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function(result) {
+		var colors = result.colors.hex;
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+
+	})
 }
